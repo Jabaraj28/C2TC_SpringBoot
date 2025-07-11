deb9ba01fbf008a1ac5bf6da89148c48efda3f9b
Jabaraj28 <sjabaraj427@gmail.com>
2025-07-11 11:13:58 +0530

    this is dependency injection project

---
+++ src/DI/src/main/java/com/tnsif/DI/Customer1.java
@@ -0,0 +1,27 @@
+package com.tnsif.DI;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="prototype")
+public class Customer1 {
+	private int cid;
+	private String cname;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	void show() {
+		System.out.println("The is from customer1 class");
+	}
+
+}
